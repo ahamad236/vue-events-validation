24e65b8a5edfd5af90a72ed58634f87183a32144
ahamad236 <ahmadshaik4u@gmail.com>
2024-07-17 14:42:30 +0530

    first commit

---
+++ src/components/FirstPageComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <h2>
+        First Page Component
+    </h2>
+    <button @click="addFn('hai')">Add</button>
+    <button @click="updateFn(10)">Update</button>
+</template>
+
+<script setup lang="ts">
+    const emits = defineEmits<{
+        (e: 'add', value: string): void
+        (e: 'update', id: number): void
+    }>()
+
+    function addFn(currentValue: string){
+        emits('add', currentValue)
+    }
+
+    function updateFn(id: number){
+        emits('update', id)
+    }
+</script>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Home Component</h1>
+    <FirstPage @add="parentAddFn" @update="parentUpdateFn"></FirstPage>
+</template>
+
+<script setup lang="ts">
+    import FirstPage from "./FirstPageComponent.vue"
+
+    function parentAddFn(value: string){
+        alert(value)
+    }
+
+    function parentUpdateFn(id: number){
+        alert(id)
+    }
+</script>
